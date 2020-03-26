b0ddc64d1127ed103d18b82a79716b5891119d3e
ChillerDragon <ChillerDragon@gmail.com>
2020-03-26 14:11:47 +0100

    Add random

--- scripts/automap_6_to_7.sh
+++ scripts/automap_6_to_7.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-RULE_FILE="${1:-generic_clear.rules}"
+# RULE_FILE="${1:-generic_clear.rules}"
+RULE_FILE="${1:-test.rules}"
 RULE_FILE_NAME="${RULE_FILE%.rules*}"
 TILESET_NAME="${2:-$RULE_FILE_NAME}"
 BASE_TILE="x"
@@ -8,13 +9,14 @@ current=-1
 declare -A R_CONDITIONS
 declare -A R_INDEX
 declare -A R_FLIP
+declare -A R_RAND
 
-if [ "$1" == "help" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ] || [ "$#" == "0" ]
-then
-    echo "usage: $(basename "$0") <0.6 rules file> [tileset name]"
-    echo "description: prints 0.6 rules json to stdout"
-    # exit 0
-fi
+# if [ "$1" == "help" ] || [ "$1" == "--help" ] || [ "$1" == "-h" ] || [ "$#" == "0" ]
+# then
+#     echo "usage: $(basename "$0") <0.6 rules file> [tileset name]"
+#     echo "description: prints 0.6 rules json to stdout"
+#     exit 0
+# fi
 
 function err() {
     echo "[-] Error: $1"
@@ -39,7 +41,7 @@ do
     if [[ "$line" =~ ^\[(.*)\] ]]
     then
         current_rule="${BASH_REMATCH[1]}"
-    elif [[ "$line" =~ ^\# ]]
+    elif [[ "$line" =~ ^\# ]] || [[ "$(echo "$line" | xargs)" == "" ]]
     then
         continue
     elif [[ "$line" =~ ^Index\ ([0-9]*)?(.*) ]]
@@ -52,52 +54,69 @@ do
         current="$((current + 1))"
         R_INDEX[$current]="${BASH_REMATCH[1]}"
         R_FLIP[$current]="${BASH_REMATCH[2]}"
-        echo "flip $current: ${R_FLIP[$current]}"
+        # echo "flip $current: ${R_FLIP[$current]}"
+    elif [[ "$line" =~ ^Random\ ([0-9-]*) ]]
+    then
+        R_RAND[$current]="${BASH_REMATCH[1]}"
     elif [[ "$line" =~ ^Pos\ ([0-9-]*)\ ([0-9-]*)\ ([A-Z]*) ]]
     then
         assert_name
         x="${BASH_REMATCH[1]}"
         y="${BASH_REMATCH[2]}"
         val="${BASH_REMATCH[3]}"
-        # TODO: NOTINDEX AND INDEX
-        # have different format
-        values='(FULL|EMPTY|NOTINDEX|INDEX)'
+        values='(FULL|EMPTY|INDEX)'
         if ! [[ "$val" =~ $values ]]
         then
             err "Invalid value '$val' at line $line_num expected $values"
             exit 1
         fi
         R_CONDITIONS[$current]+="{\"x\": $x, \"y\": $y, \"value\": \"${val,,}\"},\\n"
+    else
+        err "Invalid syntax '$line' at line $line_num"
+        exit 1
     fi
     line_num="$((line_num + 1))"
 done < "$RULE_FILE"
-
 
 (
 echo "{\"tileset\": [ { \"$TILESET_NAME\": { \"basetile\": $BASE_TILE, \"rules\": ["
 
 for r in "${!R_CONDITIONS[@]}"
 do
-    cond="${R_CONDITIONS[$r]}"
-    index="${R_INDEX[$r]}"
-    flip="${R_FLIP[$r]}"
+    i="$((r-1))"
+    i=$r
+    cond="${R_CONDITIONS[$i]}"
+    index="${R_INDEX[$i]}"
+    rand="${R_RAND[$i]}"
+    # flip="${R_FLIP[$i]}"
     echo "{"
     echo "\"index\": $index,"
-    echo "\"rule_number\": $r,"
-    echo '"condition": ['
-    echo -e "${cond::-3}"
-    printf "]"
-    if [ -z "$flip" ]
+    echo -n "\"rule_number\": $r"
+    if [ "$cond" != "" ]
+    then
+        echo ','
+        echo '"condition": ['
+        echo -e "${cond::-3}"
+        printf "]"
+    fi
+    if [ "$rand" != "" ]
+    then
+        echo ","
+        echo "\"random\": $rand"
+        # exit
+    fi
+    if [ "$flip" != "" ]
     then
         echo ","
         echo "\"${flip}\": 1"
     fi
-    printf "]}"
-    if [ "$r" -lt "$current" ]
+    printf "}"
+    if [ "$r" -eq "$current" ]
     then
         echo ","
     fi
 done
 
 echo ']}}]}'
-) # | jq
+) | jq
+
