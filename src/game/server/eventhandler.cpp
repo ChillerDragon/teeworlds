2d9a630c7997da81dc481dc9ddda134148642816
ChillerDragon <ChillerDragon@gmail.com>
2024-03-02 23:00:55 +0800

    Not sure about event handler right now
    we kinda need that to build snaps

--- src/game/server/eventhandler.cpp
+++ src/game/server/eventhandler.cpp
@@ -5,13 +5,6 @@
 #include "gamecontext.h"
 #include "player.h"
 
-//////////////////////////////////////////////////
-// Event handler
-//////////////////////////////////////////////////
-CEventHandler::CEventHandler()
-{
-}
-
 void *CEventHandler::Create(int Type, int Size, int64 Mask)
 {
 	if(m_NumEvents == MAX_EVENTS)
@@ -36,12 +29,9 @@ void CEventHandler::Snap(int SnappingClient)
 		if(SnappingClient == -1 || CmaskIsSet(m_aClientMasks[i], SnappingClient))
 		{
 			CNetEvent_Common *ev = (CNetEvent_Common *)&m_aData[m_aOffsets[i]];
-			if(SnappingClient == -1 || distance(GameServer()->m_apPlayers[SnappingClient]->m_ViewPos, vec2(ev->m_X, ev->m_Y)) < 1500.0f)
-			{
-				void *d = GameServer()->Server()->SnapNewItem(m_aTypes[i], i, m_aSizes[i]);
-				if(d)
-					mem_copy(d, &m_aData[m_aOffsets[i]], m_aSizes[i]);
-			}
+			void *d = GameServer()->Server()->SnapNewItem(m_aTypes[i], i, m_aSizes[i]);
+			if(d)
+				mem_copy(d, &m_aData[m_aOffsets[i]], m_aSizes[i]);
 		}
 	}
 }
