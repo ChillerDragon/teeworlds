d0ebc12d36a31a3d1d34acebde9abb41d145c085
ChillerDragon <ChillerDragon@gmail.com>
2024-03-10 20:05:20 +0800

    fix

--- src/packer.h
+++ src/packer.h
@@ -1,12 +1,12 @@
-#ifndef SANSIO_FLAG_TO_STR_H
-#define SANSIO_FLAG_TO_STR_H
+#ifndef SANSIO_packer_H
+#define SANSIO_packer_H
 
 #include "enums.h"
 
 /*
  * protocol: 0.6.5
  */
-unsigned char *Pack(unsigned char *pData, int Flags, int Size, int Sequence)
+unsigned char *chunk_header_pack(unsigned char *pData, int Flags, int Size, int Sequence)
 {
 	pData[0] = ((Flags&3)<<6)|((Size>>4)&0x3f);
 	pData[1] = (Size&0xf);
@@ -26,11 +26,11 @@ unsigned char *Pack(unsigned char *pData, int Flags, int Size, int Sequence)
  *
  *	int Flags, Size, Sequence;
  *	unsigned char aHeader[3] = {0x40, 0x10, 0x0a};
- *	Unpack065(aHeader, Flags, Size, Sequence);
+ *	chunk_header_unpack(aHeader, Flags, Size, Sequence);
  *
  *	printf("flags=%d size=%d sequence=%d\n", Flags, Size, Sequence);
  */
-unsigned char *Unpack(unsigned char *pData, int &Flags, int &Size, int &Sequence)
+unsigned char *chunk_header_unpack(unsigned char *pData, int &Flags, int &Size, int &Sequence)
 {
 	Flags = (pData[0]>>6)&3;
 	Size = ((pData[0]&0x3f)<<4) | (pData[1]&0xf);
